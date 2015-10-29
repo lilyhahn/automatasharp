af195199fb202cdacbe650c067aaacea11e74ae2
Tobi Hahn <rainshapes@gmail.com>
2015-10-29 17:25:33 -0400

    first

---
+++ testing/CellularAutomaton.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Paisley.CellularAutomata
+{
+    class CellularAutomaton
+    {
+        byte[] rule = new byte[8];
+        public List<byte> World { get; private set; }
+        public List<List<byte>> History { get; private set; }
+
+        public CellularAutomaton(int ruleInt, int worldSize, List<byte> initWorld = null)
+        {
+            History = new List<List<byte>>();
+            World = new List<byte>();
+            Random random = new Random();
+            if (initWorld != null)
+            {
+                World = initWorld;
+            }
+            else
+            {
+                for (int i = 0; i < worldSize; i++)
+                {
+                    World.Add((byte)random.Next(0, 2));
+                }
+            }
+            string ruleString = Convert.ToString(ruleInt, 2).PadLeft(8, '0');
+            for (int i = 0; i < ruleString.Length; i++)
+            {
+                rule[i] = (byte)Char.GetNumericValue(ruleString[i]);
+            }
+        }
+
+        public void Generate()
+        {
+            List<byte> nextWorld = new List<byte>();
+            for (int i = 0; i < World.Count; i++)
+            {
+                nextWorld.Add(0);
+            }
+            for (int i = 0; i < World.Count; i++)
+            {
+                int mcell = World[i];
+                int lcell = 0;
+                int rcell = 0;
+                if (i == 0)
+                    lcell = World[World.Count - 1];
+                else if (i == World.Count - 1)
+                    rcell = World[0];
+                else
+                {
+                    lcell = World[i - 1];
+                    rcell = World[i + 1];
+                }
+                if (lcell == 1 && rcell == 1 && mcell == 1)
+                {
+                    nextWorld[i] = rule[0];
+                }
+                else if (lcell == 1 && rcell == 0 && mcell == 1)
+                {
+                    nextWorld[i] = rule[1];
+                }
+                else if (lcell == 1 && rcell == 1 && mcell == 0)
+                {
+                    nextWorld[i] = rule[2];
+                }
+                else if (lcell == 1 && rcell == 0 && mcell == 0)
+                {
+                    nextWorld[i] = rule[3];
+                }
+                else if (lcell == 0 && rcell == 1 && mcell == 1)
+                {
+                    nextWorld[i] = rule[4];
+                }
+                else if (lcell == 0 && rcell == 0 && mcell == 1)
+                {
+                    nextWorld[i] = rule[5];
+                }
+                else if (lcell == 0 && rcell == 1 && mcell == 0)
+                {
+                    nextWorld[i] = rule[6];
+                }
+                else if (lcell == 0 && rcell == 0 && mcell == 0)
+                {
+                    nextWorld[i] = rule[7];
+                }
+            }
+            History.Add(World);
+            World = nextWorld;
+        }
+
+        new public string ToString()
+        {
+            string output = "";
+            foreach (int cell in World)
+            {
+                switch (cell)
+                {
+                    case 0:
+                        output += "0";
+                        break;
+                    case 1:
+                        output += "1";
+                        break;
+                }
+            }
+            return output;
+        }
+        public Bitmap ToImage(int pixelScale = 1, Color? cellColor = null)
+        {
+            Bitmap bmp = new Bitmap(World.Count * pixelScale, History.Count * pixelScale);
+            for (int y = 0; y < History.Count * pixelScale; y += pixelScale)
+            {
+                for (int x = 0; x < World.Count * pixelScale; x += pixelScale)
+                {
+                    Color paintColor;
+                    if (History[y / pixelScale][x / pixelScale] == 1)
+                    {
+                        paintColor = cellColor ?? Color.Black;
+                    }
+                    else
+                    {
+                        paintColor = Color.White;
+                    }
+                    for (int i = 0; i < pixelScale; i++)
+                    {
+                        for (int j = 0; j < pixelScale; j++){
+                            bmp.SetPixel(x + j, y + i, paintColor);
+                        }
+                    }
+                }
+            }
+            return bmp;
+        }
+    }
+}
---
+++ testing/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Paisley.CellularAutomata;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int width = 64;
+        int generations = 128;
+        int rule = 30;
+        int scale = 1;
+        string filename = "cells.png";
+        string defaultFilename = filename;
+
+        Console.Write("Enter the size of the world. [" + width + "] ");
+        int defaultWidth = width;
+        if (!Int32.TryParse(Console.ReadLine(), out width))
+            width = defaultWidth;
+
+        Console.Write("Enter the desired number of generations to run. [" + generations + "] ");
+        int defaultGenerations = generations;
+        if (!Int32.TryParse(Console.ReadLine(), out generations))
+            generations = defaultGenerations;
+
+        Console.Write("Enter the rule to use. [" + rule + "] ");
+        int defaultRule = rule;
+        if (!Int32.TryParse(Console.ReadLine(), out rule))
+            rule = defaultRule;
+
+        Console.Write("Enter the path where the generated image should be saved. [" + filename + "] ");
+        filename = Console.ReadLine();
+
+        Console.Write("Enter image scale multiplier. [" + scale + "] ");
+        int defaultScale = scale;
+        if (!Int32.TryParse(Console.ReadLine(), out scale))
+            scale = defaultScale;
+
+        CellularAutomaton ca = new CellularAutomaton(rule, width);
+        Console.Write("Enter \"r\" for random starting seed or \"s\" for single cell. [r] ");
+        string choice = Console.ReadLine();
+        if (choice == "s")
+        {
+            List<byte> startingField = new List<byte>();
+            for (int i = 0; i < width; i++)
+            {
+                startingField.Add(0);
+                if (Math.Floor(width / 2f) == i)
+                {
+                    startingField[i] = 1;
+                }
+            }
+            ca = new CellularAutomaton(rule, width, startingField);
+        }
+
+        for (int i = 0; i < generations; i++)
+        {
+            ca.Generate();
+            Console.WriteLine(ca.ToString());
+        }
+        try
+        {
+            ca.ToImage().Save(filename);
+        }
+        catch(ArgumentException)
+        {
+            ca.ToImage(scale).Save(defaultFilename);
+        }
+    }
+}
